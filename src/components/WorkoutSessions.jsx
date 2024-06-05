9a909b120cf01a090d2b56b081fd0d51b255b090
Papiya Dutta <118613949+PAPIYA-DUTTA@users.noreply.github.com>
2024-06-05 15:29:11 +0530

    Update WorkoutSessions.jsx

--- src/components/WorkoutSessions.jsx
+++ src/components/WorkoutSessions.jsx
@@ -9,7 +9,7 @@ const WorkoutSessions = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur,
           quisquam. Eaque alias, blanditiis quidem maiores illo unde ipsam!
         </p>
-        <img src="/img5.jpg" alt="workout" />
+        <img src="public/img5.jpg" alt="workout" />
       </div>
       <div className="wrapper">
         <h1>FEATURED BOOTCAMPS</h1>
